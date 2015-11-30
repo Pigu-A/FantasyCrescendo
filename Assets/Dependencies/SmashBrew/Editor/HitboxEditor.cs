e07dfb00a9ac0bc2e12a80878bca66605f14a55b
james7132 <james.siqi.liu@gmail.com>
2015-11-29 20:24:39 -0500

    Hitbox Add Undo and Keyboard Shortcut

--- Assets/Dependencies/SmashBrew/Editor/HitboxEditor.cs
+++ Assets/Dependencies/SmashBrew/Editor/HitboxEditor.cs
@@ -1,22 +1,29 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityConstants;
+using UnityEditor;
 using UnityEngine;
 
 namespace Hourai.SmashBrew.Editor {
 
     public class HitboxEditor {
 
-        [MenuItem("Hourai/Hitbox/Add")]
+        [MenuItem("GameObject/Hitbox %h", false, 5)]
         public static void AddHitbox() {
+            var createdHitboxes = new List<Object>();
             foreach (var go in Selection.gameObjects) {
                 if (!go)
                     continue;
-                var hitbox = new GameObject("Hitbox", typeof(SphereCollider), typeof(Hitbox));
-                hitbox.AddComponent<Hitbox>();
+                var hitbox = new GameObject("Hitbox", typeof (SphereCollider), typeof (Hitbox)) {
+                    tag = Tags.Hitbox,
+                    layer = Layers.Hitbox
+                };
                 hitbox.transform.parent = go.transform;
                 hitbox.transform.localPosition = Vector3.zero;
-                hitbox.transform.rotation = Quaternion.identity;
-                Selection.activeGameObject = hitbox;
+                hitbox.transform.localEulerAngles = Vector3.zero;
+                createdHitboxes.Add(hitbox);
+                Undo.RegisterCreatedObjectUndo(hitbox, "Create Hitbox");
             }
+            Selection.objects = createdHitboxes.ToArray();
         }
 
     }
